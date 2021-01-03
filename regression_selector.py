1b3f5757318e69b62be45c7ccb6199d5805665ec
azizmousa <azizmousa1010@gmail.com>
2021-01-03 06:40:09 +0200

    add the sets getter and setters

--- regression_selector.py
+++ regression_selector.py
@@ -11,12 +11,20 @@ from random_forest_regression_model import RandomForestRegressionModel
 class RegressionSelector:
     __models = []
     __adjust_r_square_arr = []
+    __x_train = None
+    __y_train = None
+    __x_validation = None
+    __y_validation = None
 
     def __init__(self, x_train, y_train, x_validation=None, y_validation=None,
                  linear_model=None, polynomial_model=None, svr_model=None, random_forest_model=None,
                  regression_tree_model=None):
         self.__models = []
         self.__adjust_r_square_arr = []
+        self.__x_train = x_train
+        self.__x_validation = x_validation
+        self.__y_train = y_train
+        self.__y_validation = y_validation
 
         if linear_model is None:
             linear_model = LinearRegressionModel(x_train=x_train, y_train=y_train,
@@ -53,3 +61,27 @@ class RegressionSelector:
         if not isinstance(model, RegressionModel):
             raise TypeError("model should be type of RegressionModel")
         self.__models.append(model)
+
+    def set_x_trainset(self, x_train):
+        self.__x_train = x_train
+
+    def set_y_trainset(self, y_train):
+        self.__y_train = y_train
+
+    def set_x_validationset(self, x_validation):
+        self.__x_validation = x_validation
+
+    def set_y_validationset(self, y_validation):
+        self.__y_validation = y_validation
+
+    def get_x_trainset(self):
+        return self.__x_train
+
+    def get_y_trainset(self):
+        return self.__y_train
+
+    def get_x_validationset(self):
+        return self.__x_validation
+
+    def get_y_validationset(self):
+        return self.__y_validation
