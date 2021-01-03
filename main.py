e92428a266659e11e499367a9e747aaf35ddc8f8
azizmousa <azizmousa1010@gmail.com>
2021-01-03 23:30:39 +0200

    print the best fit model and its r2 value

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from regression_selector import RegressionSelector
 
-dataset = pd.read_csv("50_Startups.csv")
+
+dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 y = y.reshape(-1, 1)
 
-clt = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [3])], remainder="passthrough")
-X = clt.fit_transform(X)
+# clt = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [3])], remainder="passthrough")
+# X = clt.fit_transform(X)
 
-x_tarin, x_test, y_train, y_test = train_test_split(X, y, random_state=1)
+x_tarin, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 selector = RegressionSelector(x_train=x_tarin, y_train=y_train, x_validation=x_test, y_validation=y_test)
 selector.start_evaluation()
 print(selector.get_evaluation_array())
-print("Best Model Fit your Dataset is:", type(selector.get_best_fit_model()))
+best_model = selector.get_best_fit_model()
+print(f"Best Model Fit your Dataset is: {type(best_model[0])} with adjust r^2 = {best_model[1]}")
+
