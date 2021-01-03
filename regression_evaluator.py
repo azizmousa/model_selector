correct calculation bug

regression_evaluator.py
@@ -4,12 +4,12 @@ import numpy as np
 class RegressionEvaluator:
 
     @staticmethod
-    def adjust_r_squar_error(self, model, x_train, y_train):
-        y_hat = model.predicit(x_train)
-        ss_resduals = sum((y_train - y_hat)**2)
-        ss_total = sum((y_train - np.mean(y_train))**2)
+    def adjust_r_squar_error(model, x_validation, y_validation):
+        y_hat = model.predict(x_validation)
+        ss_resduals = sum((y_validation.flatten() - y_hat.flatten()) ** 2)
+        ss_total = sum((y_validation.flatten() - np.mean(y_validation)) ** 2)
         r_squar = 1 - (float(ss_resduals)/ss_total)
-        n = len(y_train)
-        p = len(x_train[0])
+        n = len(y_validation)
+        p = len(x_validation[0])
         adjust_r_squar = 1 - (1 - r_squar)*((n-1) / (n-p-1))
         return adjust_r_squar