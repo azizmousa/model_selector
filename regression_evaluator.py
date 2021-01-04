c69a9612c3bf9fc71455f1e9d33c4a269365da99
azizmousa <azizmousa1010@gmail.com>
2021-01-04 05:57:59 +0200

    implement mean absolute error evaluation function

--- regression_evaluator.py
+++ regression_evaluator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import mean_absolute_error
 
 
 class RegressionEvaluator:
@@ -8,8 +9,13 @@ class RegressionEvaluator:
         y_hat = model.predict(x_validation)
         ss_resduals = sum((y_validation.flatten() - y_hat.flatten()) ** 2)
         ss_total = sum((y_validation.flatten() - np.mean(y_validation)) ** 2)
-        r_squar = 1 - (float(ss_resduals)/ss_total)
+        r_squar = 1 - (float(ss_resduals) / ss_total)
         n = len(y_validation)
         p = len(x_validation[0])
-        adjust_r_squar = 1 - (1 - r_squar)*((n-1) / np.abs(n-p-1))
+        adjust_r_squar = 1 - (1 - r_squar) * ((n - 1) / np.abs(n - p - 1))
         return adjust_r_squar
+
+    @staticmethod
+    def get_mean_absolute_error(model, x_validation, y_validation):
+        y_hat = model.predict(x_validation)
+        return mean_absolute_error(y_validation, y_hat)
